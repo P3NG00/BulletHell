a88035651febdde33c5aa89e65b76ab9ba8f3d0f
P3NG00 <p3ng00minecraft@gmail.com>
2023-01-28 18:05:13 -0700

    removed iequatable from gameobjects

--- src/game/GameObject.cs
+++ src/game/GameObject.cs
@@ -1,39 +1,9 @@
-using System;
-
 namespace BulletHell.Game
 {
-    public abstract class GameObject : IEquatable<GameObject>
+    public abstract class GameObject
     {
         public readonly int ID;
 
         public GameObject(int id) => ID = id;
-
-        public bool Equals(GameObject other)
-        {
-            if (ReferenceEquals(other, null))
-                return false;
-            if (ReferenceEquals(this, other))
-                return true;
-            return ID == other.ID;
-        }
-
-        public sealed override bool Equals(object obj) => Equals(obj as GameObject);
-
-        public sealed override int GetHashCode() => ID.GetHashCode() ^ base.GetHashCode();
-
-        public static bool operator ==(GameObject a, GameObject b)
-        {
-            bool aNull = ReferenceEquals(a, null);
-            bool bNull = ReferenceEquals(b, null);
-            if (aNull && bNull)
-                return true;
-            if (aNull || bNull)
-                return false;
-            if (ReferenceEquals(a, b))
-                return true;
-            return a.Equals(b);
-        }
-
-        public static bool operator !=(GameObject a, GameObject b) => !(a == b);
     }
 }
