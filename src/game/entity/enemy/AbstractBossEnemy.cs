592c165f998b966f68e725fe7356c08bd5aa1363
P3NG00 <p3ng00minecraft@gmail.com>
2023-12-23 00:36:14 -0700

    restricted access

--- src/game/entity/enemy/AbstractBossEnemy.cs
+++ src/game/entity/enemy/AbstractBossEnemy.cs
@@ -3,9 +3,9 @@ using Microsoft.Xna.Framework;
 
 namespace BulletHell.Game.Entities.Enemies
 {
-    public sealed class AbstractBossEnemy : AbstractEnemy
+    public abstract class AbstractBossEnemy : AbstractEnemy
     {
-        public AbstractBossEnemy(
+        protected AbstractBossEnemy(
             Vector2 position,
             float radius,
             float moveSpeed,
